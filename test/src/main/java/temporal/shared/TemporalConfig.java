d705bcdea703cc0682e5385e1f096c039f90fac4
Christophe De Backer <christophe.db@live.be>
2022-10-13 20:37:39 +0200

    typo fix

--- test/src/main/java/temporal/shared/TemporalConfig.java
+++ test/src/main/java/temporal/shared/TemporalConfig.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class TemporalConfig {
 	//the docker-compose image will use this as default address
 	public static final String ADDR = "localhost";
-	public static final String PORT = "8080";
+	public static final String PORT = "7233";
 	//default domain
 	public static final String DOMAIN = "default";
 	//queue where the "helloworld workflow" places workflows to be executed & where workers will go find them
